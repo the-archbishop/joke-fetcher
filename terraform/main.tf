d99d4109316805b07c598c6f0f717bf66be8c825
Bishop Bader <bflatbader@gmail.com>
2024-12-12 11:26:58 -0500

    namespace was missing

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,12 +2,23 @@ provider "kubernetes" {
   config_path = "~/.kube/config"
 }
 
-# Apply the Deployment YAML file
-resource "kubernetes_manifest" "joke_fetcher_deployment" {
-  manifest = yamldecode(file("${path.module}/../k8s/deployment.yaml"))
+# Create the namespace
+resource "kubernetes_namespace" "joke_fetcher" {
+  metadata {
+    name = "joke-fetcher"
+  }
 }
 
-# Apply the Service YAML file
+# Apply the Deployment YAML
+resource "kubernetes_manifest" "joke_fetcher_deployment" {
+  manifest = yamldecode(file("${path.module}/../k8s/deployment.yaml"))
+
+  depends_on = [kubernetes_namespace.joke_fetcher]
+}
+
+# Apply the Service YAML
 resource "kubernetes_manifest" "joke_fetcher_service" {
   manifest = yamldecode(file("${path.module}/../k8s/service.yaml"))
+
+  depends_on = [kubernetes_namespace.joke_fetcher]
 }
