f6b3e24ef1c997a2ce255c07b5adf1b7827e14ff
Bishop Bader <bflatbader@gmail.com>
2024-12-13 12:21:11 -0500

    trying to get karpenter replication working

--- terraform/karpenter.tf
+++ terraform/karpenter.tf
@@ -2,8 +2,16 @@ resource "kubectl_manifest" "karpenter" {
   yaml_body = file("${path.module}/../config/karpenter_namespace.yaml")
 }
 
+resource "kubectl_manifest" "karpenter_secrets" {
+  yaml_body = file("${path.module}/../config/karpenter_secrets.yaml")
+
+  depends_on = [kubectl_manifest.karpenter]
+}
+
 resource "kubectl_manifest" "karpenter_provisioner" {
   yaml_body = file("${path.module}/../config/karpenter_provisioner.yaml")
+
+  depends_on = [kubectl_manifest.karpenter_secrets]
 }
 
 resource "kubectl_manifest" "karpenter_service_account" {
