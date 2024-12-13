f3b8a4cd421c282779ba748b2bf3a06c0594c124
Bishop Bader <bflatbader@gmail.com>
2024-12-12 21:44:17 -0500

    missing karpenter service account

--- terraform/karpenter.tf
+++ terraform/karpenter.tf
@@ -6,6 +6,12 @@ resource "kubectl_manifest" "karpenter_provisioner" {
   yaml_body = file("${path.module}/../config/karpenter_provisioner.yaml")
 }
 
+resource "kubectl_manifest" "karpenter_service_account" {
+  yaml_body = file("${path.module}/../config/karpenter_service_account.yaml")
+}
+
 resource "kubectl_manifest" "karpenter_deployment" {
   yaml_body = file("${path.module}/../config/karpenter_deployment.yaml")
+
+  depends_on = [kubectl_manifest.karpenter_service_account]
 }
