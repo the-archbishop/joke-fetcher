232749c0b4ea5fe5c3c11744d8d152012fae7123
Bishop Bader <bflatbader@gmail.com>
2024-12-12 21:07:35 -0500

    missing karpenter namespace, added deployment

--- terraform/karpenter.tf
+++ terraform/karpenter.tf
@@ -1,3 +1,11 @@
+resource "kubectl_manifest" "karpenter" {
+  yaml_body = file("${path.module}/../config/karpenter_namespace.yaml")
+}
+
 resource "kubectl_manifest" "karpenter_provisioner" {
   yaml_body = file("${path.module}/../config/karpenter_provisioner.yaml")
+}
+
+resource "kubectl_manifest" "karpenter_deployment" {
+  yaml_body = file("${path.module}/../config/karpenter_deployment.yaml")
 }
